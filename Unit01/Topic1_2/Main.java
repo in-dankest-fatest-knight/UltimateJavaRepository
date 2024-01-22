40d1f4375eb3427fd5e8b493e992f87c345ceb03
Logan Byerly <loganbyerly6@gmail.com>
2024-01-22 09:24:25 -0600

    comments

--- Unit01/Topic1_2/Main.java
+++ Unit01/Topic1_2/Main.java
@@ -2,7 +2,18 @@ package Unit01.Topic1_2;
 
 public class Main {
   public static void main(String[] args) {
-    // Your code goes here
+    int a = 4;
+
+    int b = 5;
+    
+    a++;
+    
+    b++;
+    
+    int c = a + b;
+    
+    a -= 1;
+    
+    System.out.println(a + c);
   }
-  
 }
