8bee9e8cb4303051e7528ba3a2667b47539e5aa4
Logan Byerly <loganbyerly6@gmail.com>
2024-01-29 09:10:13 -0600

    2.2 changes

--- Unit02/Topic2_2/Main.java
+++ Unit02/Topic2_2/Main.java
@@ -1,8 +1,15 @@
 package Unit02.Topic2_2;
 
+import java.util.Random;
+
 public class Main {
   public static void main(String[] args) {
+
+    Random geny = new Random();
+  
+    String bruh = "apcsa";
+    int boi = (bruh.length());
+    int rand = geny.nextInt(boi);
     
   }
-
 }
