a1aab413cbfdede67841da1cf0de0b465dd038d4
Logan Byerly <loganbyerly6@gmail.com>
2024-02-13 09:11:29 -0600

    stringloops stuff

--- Unit04/StringLoops.java
+++ Unit04/StringLoops.java
@@ -41,30 +41,70 @@ public class StringLoops {
 
     public static String doubleChar(String str) {
         String nstr = "";
-        for(int i = 0; i <= str.length(); i++){
+        for(int i = 0; i < str.length(); i++){
             nstr += str.substring(i, i + 1) + str.substring(i, i + 1);
         }
         return nstr;
     }
 
     public static int countCode(String str) {
-        return 0;
+        int count = 0;
+        for(int i = 0; i <= str.length() - 4; i++){
+            if(str.substring(i, i + 1).equals("c") && str.substring(i + 1, i + 2).equals("o") && str.substring(i + 3, i + 4).equals("e")){
+                count++;
+            }
+        }
+        return count;
     }
 
     public static boolean bobThere(String str) {
+        for(int i = 0; i <= str.length() - 3; i++){
+            if(str.substring(i, i + 1).equals("b") && str.substring(i + 2, i + 3).equals("b")){
+                return true;
+            }
+        }
         return false;
     }
 
     public static boolean prefixAgain(String str, int n) {
+        String pre = str.substring(0, n);
+        for(int i = 1; i <= str.length() - n; i++){
+            if(str.substring(i, i + n).equals(pre)){
+                return true;
+            }
+        }
         return false;
     }
 
     public static boolean sameStarChar(String str) {
-        return false;
+        for(int i = 1; i <= str.length() - 2; i++){
+            if(str.substring(i, i + 1).equals("*")){
+                if(!(str.substring(i - 1, i).equals(str.substring(i + 1, i + 2)))){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     public static String starOut(String str) {
-        return "";
+        String nstr = "";
+        for(int i = 0; i <= str.length(); i++){
+            if(i == 0){
+                if(!str.substring(i, i + 1).equals("*") && !str.substring(i + 1, i + 2).equals("*")){
+                    nstr += str.substring(i, i + 1);
+                }
+            }
+            else if(!str.substring(i - 1, i).equals("*") && !str.substring(i, i + 1).equals("*")){
+                nstr += str.substring(i, i + 1);
+            }
+            else{
+                if(!str.substring(i - 1, i).equals("*") && !str.substring(i, i + 1).equals("*") && !str.substring(i + 1, i + 2).equals("*")){
+                    nstr += str.substring(i, i + 1);
+                }
+            }
+        }
+        return nstr;
     }
 
     public static int countHi(String str) {
