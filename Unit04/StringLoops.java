27cd7d29c9fb2295c07138922ee9367d55426ee9
Logan Byerly <loganbyerly6@gmail.com>
2024-02-13 10:29:25 -0600

    stringloops changes

--- Unit04/StringLoops.java
+++ Unit04/StringLoops.java
@@ -89,37 +89,142 @@ public class StringLoops {
 
     public static String starOut(String str) {
         String nstr = "";
-        for(int i = 0; i <= str.length(); i++){
-            if(i == 0){
-                if(!str.substring(i, i + 1).equals("*") && !str.substring(i + 1, i + 2).equals("*")){
-                    nstr += str.substring(i, i + 1);
+        //handling for short strings
+        if(str.length() <= 3){
+            if(str.length() == 0){
+                return "";
+            }
+            else if(str.length() == 1){
+                if(str.substring(0, 1).equals("*")){
+                    nstr = "";
+                }
+                else{
+                    nstr = str;
                 }
             }
-            else if(!str.substring(i - 1, i).equals("*") && !str.substring(i, i + 1).equals("*")){
-                nstr += str.substring(i, i + 1);
+            else if(str.length() == 2){
+                if(str.substring(0, 1).equals("*") || str.substring(1, 2).equals("*")){
+                    nstr = "";
+                }
+                else{
+                    nstr = str;
+                }
             }
-            else{
-                if(!str.substring(i - 1, i).equals("*") && !str.substring(i, i + 1).equals("*") && !str.substring(i + 1, i + 2).equals("*")){
-                    nstr += str.substring(i, i + 1);
+            else if(str.length() == 3){
+                if(str.substring(0, 1).equals("*") || str.substring(1, 2).equals("*") || str.substring(2, 3).equals("*")){
+                    nstr = "";
+                }
+                else{
+                    nstr = str;
                 }
             }
         }
+        else{
+            for(int i = 0; i < str.length(); i++){
+                if(i == 0){
+                    if(!str.substring(i, i + 1).equals("*") && !str.substring(i + 1, i + 2).equals("*")){
+                        nstr += str.substring(i, i + 1);
+                    }
+                }
+                else if(i == str.length() - 1){
+                    if(!str.substring(i - 1, i).equals("*") && !str.substring(i, i + 1).equals("*")){
+                        nstr += str.substring(i, i + 1);
+                    }
+                }
+                else{
+                    if(!str.substring(i - 1, i).equals("*") && !str.substring(i, i + 1).equals("*") && !str.substring(i + 1, i + 2).equals("*")){
+                      nstr += str.substring(i, i + 1);
+                    }
+                }
+            }
+        }
+
         return nstr;
     }
 
     public static int countHi(String str) {
-        return 0;
+        int count = 0;
+        if(str.length() <= 2){
+            if(str.equals("hi")){
+                count++;
+            }
+        }
+        else{
+            for(int i = 0; i <= str.length() - 2; i++){
+                if(str.substring(i, i + 2).equals("hi")){
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 
     public static boolean endOther(String a, String b) {
-        return false;
+        String A = a.toLowerCase();
+        String B = b.toLowerCase();
+
+        if(A.length() > B.length()){
+            if(A.substring(A.length() - B.length()).equals(B)){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else if(B.length() > A.length()){
+            if(B.substring(B.length() - A.length()).equals(A)){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            if(a.equals(B)){
+                return true;
+            }
+            else{
+                return false;
+
+            }
+        }
     }
 
     public static boolean xyBalance(String str) {
-        return false;
+        boolean holder = true;
+        if(str.length() == 1){
+            if(str.equals("x")){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+        else{
+            for(int i = 0; i < str.length(); i++){
+                if(str.substring(i, i + 1).equals("x")){
+                    holder = false;
+                }
+                else if(str.substring(i, i + 1).equals("y")){
+                    holder = true;
+                }
+            }
+        }
+        return holder;
     }
 
     public static String plusOut(String str, String word) {
+        String nstr = "";
+        if(str.length() <= word.length()){
+            if(str.equals(word)){
+                return str;
+            }
+            else{
+                for(int i = 0; i <= str.length(); i++){
+                    
+                }
+            }
+        }
         return "";
     }
 
@@ -133,5 +238,9 @@ public class StringLoops {
 
     public static String repeatSeparator(String word, String sep, int count) {
         return "";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(starOut("ab*cd"));
     }
 }
--- Unit04/Topic4_1WhileLoops.java
+++ Unit04/Topic4_1WhileLoops.java
@@ -2,7 +2,8 @@ package Unit04;
 
 public class Topic4_1WhileLoops {
     public static void main(String[] args) {
-
+        // print last letter
+        System.out.println("star".substring(3,4));
     }
 }
 // javac Unit04\Topic4_1WhileLoops.java; java Unit04.Topic4_1WhileLoops
