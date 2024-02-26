ef52da213e18854a6a6b16a69fe85173638d21bb
Logan Byerly <loganbyerly6@gmail.com>
2024-02-26 09:12:59 -0600

    u3 stuff

--- Unit03/Topic3_7/Main.java
+++ Unit03/Topic3_7/Main.java
@@ -1,7 +1,23 @@
 package Unit03.Topic3_7;
 
 public class Main {
+
+  public static boolean almostEquals(double n1, double n2, double tolerance) {
+    return Math.abs(n1 - n2) < tolerance;
+  }
+
   public static void main(String[] args) {
+    double num1 = 1.0;
+
+    double num2 = 0.0;
+
+    for (int i = 1; i<=10; i++) {
+      num2 += 0.1;
+    }
+
+    System.out.println("num1: " + num1 + ", num2: " + num2);
+    System.out.println(num1 == num2);
+    System.out.println(almostEquals(num1, num2, 1e-9));
 
   }
 
