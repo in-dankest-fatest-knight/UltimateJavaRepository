b720d386b9f4a53f5bdca5d6ccbaa1b25215d88d
Logan Byerly <loganbyerly6@gmail.com>
2024-05-07 09:14:07 -0500

    experiment

--- Unit09/Topic9_2WritingConstructorsForSubclasses.java
+++ Unit09/Topic9_2WritingConstructorsForSubclasses.java
@@ -2,6 +2,13 @@ package Unit09;
 
 public class Topic9_2WritingConstructorsForSubclasses {
     public static void main(String[] args) {
+        double x = 0.0;
+    
+        for (int i = 0; i < 10; i++) {
+            x += 0.1;
+        }
 
+        System.out.println("x = " + x);
+        System.out.println(x == 1.0);
     }
 }
